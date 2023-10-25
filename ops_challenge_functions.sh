a34020128d7caad7d01f5ff3b39ef85b762679ec
Raymond Mankin <rdmankin@gmail.com>
2023-10-25 19:02:26 -0400

    Ops Challenge 03

---
+++ ops_challenge_functions.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Script Name: Ops Challenge Functions
+
+# Author: Raymond Mankin
+
+# Date of last revision: 10/25/2023
+
+# Purpose: Bash Script Intro
+
+
+# Declaration of variables
+
+
+# Declaration of functions
+
+
+# Main 
+#!/bin/bash
+display_login_history() {
+  echo "Login History:"
+  last
+}
+
+
+for i in {1..3}; do
+  display_login_history
+done
+
+
+
+
+
+
+# End

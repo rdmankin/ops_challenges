50a2764f4c645377bc0eb907ddf3df551198f72d
Raymond Mankin <rdmankin@gmail.com>
2023-10-25 16:23:21 -0400

    helloworld

--- helloworld.sh
+++ helloworld.sh
@@ -1,1 +1,24 @@
-Script1
+#!/bin/bash
+
+# Script Name: helloworld.sh
+
+# Author: Raymond Mankin
+
+# Date of last revision: 10/25/2023
+
+# Purpose: Bash Script Intro
+
+
+# Declaration of variables
+
+
+# Declaration of functions
+
+
+# Main
+#!/bin/bash
+echo "Hello, World!"
+Hello, World!
+
+
+# End

16077eea469afe8f6cec3f0c55975e66bebae21b
Raymond Mankin <rdmankin@gmail.com>
2023-10-31 20:22:32 -0400

    grep_lshw

---
+++ lshw_grep.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Script Name: lshw_grep.sh
+
+# Author: Raymond Mankin
+
+# Date of last revision: 10/27/2023
+
+# Purpose: Report Current CPU and RAM Performance
+
+## Computer name: ram85-OptiPlex-790
+
+## CPU information:
+  Product: 
+  Vendor: 
+  Physical ID: 
+  Bus info: 
+  Width: 
+
+## RAM information:  
+Size: 
+  Description: 
+  Physical ID: 
+  Size: 
+  Description: 
+  Physical ID: 
